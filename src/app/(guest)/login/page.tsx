4e698b20c61e4feca07073cc9de565b8ec8ae113
abdlhaklalouche <abdlhaklalouche@gmail.com>
2024-12-31 08:35:09 +0300

    Add form validations components

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -1,17 +1,3 @@
-// import LoginForm from "./form";
-
-// export default () => {
-//   return (
-//     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-//       <div className="w-full max-w-sm">
-//         <LoginForm />
-//       </div>
-//     </div>
-//   );
-// };
-
-import { GalleryVerticalEnd } from "lucide-react";
-
 import LoginForm from "./form";
 
 export default function LoginPage() {
---
+++ src/app/components/form/textfield.tsx
@@ -0,0 +1,75 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+import { cn } from "@/lib/utils";
+import {
+  ControllerRenderProps,
+  FieldValues,
+  useFormContext,
+} from "react-hook-form";
+
+type FormInputProps = {
+  name: string;
+  label?: string;
+  description?: string;
+} & React.ComponentProps<"input">;
+
+const FormTextField: React.FC<FormInputProps> = ({
+  name,
+  label,
+  description,
+  ...otherProps
+}) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    field: ControllerRenderProps<FieldValues, string>
+  ) => {
+    field.onChange(e.target.value);
+  };
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      defaultValue={otherProps.defaultValue ?? ""}
+      render={({ field }) => (
+        <FormItem className="space-y-0 w-full">
+          {label && (
+            <FormLabel className="text-2xs text-gray-500">{label}</FormLabel>
+          )}
+          <FormControl>
+            <Input
+              autoComplete="off"
+              {...otherProps}
+              className={cn("h-8 text-xs w-52", otherProps.className)}
+              defaultValue={otherProps.defaultValue}
+              {...field}
+              onChange={(e) => handleChange(e, field)}
+            />
+          </FormControl>
+          {description && (
+            <FormDescription className="text-2xs">
+              {description}
+            </FormDescription>
+          )}
+          <FormMessage className="text-3xs" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormTextField;
